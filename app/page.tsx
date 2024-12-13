556ff0484695f8164662f9eebfadd495098d580d
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2024-12-13 23:15:44 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import "./style/home.css"
 import Home from "./components/Home"
 import Header from "./components/Header"
-import Cat from "./Categ/page"
 function Page(){
   return(
   <div className="main">
    <Header />
-   {/* <Cat /> */}
    <Home />
   </div>)
 }export default Page
