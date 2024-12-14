80fd6dea45cf5dae7ddbd76dab0e2d609dbb7164
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2024-12-15 01:34:39 +0500

    Add files via upload

---
+++ app/Detail/page.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image"
+import Header from "../components/Header"
+import Foot from "../components/Footer"
+
+function Detail(){
+    return(
+        <div className="w-[1440px] h-[3066px]" >
+            <Header /> 
+            <div className="ml-[100px] mt-[15px]">
+            <Image src="/images2/Line2.png" alt="line" width={1240} height={1}/>
+            </div>
+            <div className="ml-[100px] mt-[30px]">
+            <Image src="/images3/t-shirts.png" alt="arrow" width={259} height={22}/>
+            </div>
+            <div className="ml-[100px] mt-[35px] flex items-start">
+            <Image src="/images3/desi-one.png" alt="shirt" width={152} height={167}/>
+            <Image src="/images3/main.png" alt="shirt" width={444} height={530} className="ml-[10px]"/>
+            <Image src="/images3/One Life Graphic T-shirt.png" alt="heading" width={600} height={48} className="ml-[30px]"/>
+            <Image src="/images3/desi-two.png" alt="shirt" width={152} height={168} className="ml-[-1237px] mt-[182px]"/>
+            <Image src="/images3/boy-s.png" alt="shirt" width={152} height={167} className="ml-[-150px] mt-[362px]"/>
+            <Image src="/images2/stars.png" alt="stars" width={193} height={25} className="ml-[480px] mt-[50px]"/>
+            <Image src="/images3/price.png" alt="rate" width={176} height={43} className="ml-[-190px] mt-[82px]"/>
+            <Image src="/images3/off-two.png" alt="off" width={72} height={34} className="ml-[15px] mt-[85px]"/>
+            <Image src="/images3/text.png" alt="text" width={590} height={33} className="ml-[-260px] mt-[145px]"/>
+            <Image src="/images3/Line 1.png" alt="line" width={590} height={1} className="ml-[-590px] mt-[210px]"/>
+            <Image src="/images3/Select Colors.png" alt="select" width={93} height={11} className="ml-[-590px] mt-[235px]"/>
+            <Image src="/images3/colors.png" alt="colors" width={143} height={37} className="ml-[-95px] mt-[265px]"/>
+            <Image src="/images3/Line 1.png" alt="line" width={590} height={1} className="ml-[-145px] mt-[325px]"/>
+            <Image src="/images3/Choose Size.png" alt="size" width={87} height={11} className="ml-[-590px] mt-[350px]"/>
+            <Image src="/images3/size.png" alt="size" width={420} height={46} className="ml-[-90px] mt-[380px]"/>
+            <Image src="/images3/Line 1.png" alt="line" width={590} height={1} className="ml-[-412px] mt-[455px]"/>
+            <Image src="/images3/pgs.png" alt="no" width={170} height={52} className="ml-[-600px] mt-[480px]"/>
+            <Image src="/images3/add.png" alt="cart" width={400} height={52} className="ml-[30px] mt-[480px]"/>
+            </div>
+            <div className="flex items-start mt-[70px] ml-[100px]">
+            <Image src="/images3/Product Details.png" alt="text" width={133} height={14} className="ml-[135px]"/>
+            <Image src="/images3/Rating & Reviews.png" alt="text" width={154} height={14} className="ml-[260px]"/>
+            <Image src="/images3/FAQs.png" alt="text" width={47} height={14} className="ml-[310px]"/>
+            </div>
+            <div className="flex items-start mt-[20px]">
+            <Image src="/images3/Line 5.png" alt="line" width={1240} height={1} className="mt-[2px] ml-[100px]"/>
+            <Image src="/images3/Line 7.png" alt="line" width={414} height={1} className="ml-[-830px]"/>
+            </div>
+            <div className="flex items-start mt-[50px]">
+            <Image src="/images3/All Reviews.png" alt="text" width={125} height={32} className="ml-[100px]"/>
+            <Image src="/images3/(451).png" alt="no" width={33} height={11} className="ml-[10px]"/>
+            <Image src="/images3/latest.png" alt="image" width={354} height={48} className="ml-[710px] mt-[-17px]"/>
+            </div>
+           <div className="flex items-start ml-[100px] mt-[30px]">
+           <Image src="/images3/saman.png" alt="image" width={610} height={241}/>
+           <Image src="/images3/alex.png" alt="image" width={610} height={241} className="ml-[15px]"/>
+           </div>
+           <div className="flex items-start ml-[100px] mt-[20px]">
+           <Image src="/images3/ethan.png" alt="image" width={610} height={241}/>
+           <Image src="/images3/olivia.png" alt="image" width={610} height={241} className="ml-[15px]"/>
+           </div>
+           <div className="flex items-start ml-[100px] mt-[20px]">
+           <Image src="/images3/liam.png" alt="image" width={610} height={241}/>
+           <Image src="/images3/ava.png" alt="image" width={610} height={241} className="ml-[15px]"/>
+           </div>
+           <div className="flex justify-center items-center mt-[40px]">
+           <Image src="/images3/load.png" alt="button" width={230} height={52}/>
+           </div>
+           <div className="flex justify-center items-center mt-[60px]">
+           <Image src="/images3/heading.png" alt="heading" width={579} height={58}/>
+           </div>
+           <div className="flex items-start ml-[100px] mt-[50px]">
+           <Image src="/images3/blue.png" alt="shirt" width={295} height={298}/>
+           <Image src="/images3/g-pink.png" alt="shirt" width={295} height={298} className="ml-[18px]"/>
+           <Image src="/images3/collar.png" alt="shirt" width={295} height={298} className="ml-[18px]"/>
+           <Image src="/images3/w-strip.png" alt="shirt" width={295} height={298} className="ml-[18px]"/>
+           </div>
+           <div className="flex items-start ml-[100px] mt-[20px]">
+           <Image src="/images3/text1.png" alt="text" width={228} height={27}/>
+           <Image src="/images2/Gradient Graphic T-shirt.png" alt="text" width={227} height={27} className="ml-[80px]"/>
+           <Image src="/images2/Polo with Tipping Details.png" alt="text" width={231} height={27} className="ml-[90px]"/>
+           <Image src="/images2/Black Striped T-shirt.png" alt="text" width={190} height={27} className="ml-[90px]"/>
+           </div>
+           <div className="flex items-start ml-[100px] mt-[10px]">
+           <Image src="/images2/star2.png" alt="stars" width={137} height={19}/>
+           <Image src="/images2/star.png" alt="stars" width={126} height={19} className="ml-[170px]"/>
+           <Image src="/images2/stars.png" alt="stars" width={150} height={19} className="ml-[190px]"/>
+           <Image src="/images2/star1.png" alt="stars" width={160} height={19} className="ml-[170px]"/>
+           </div>
+           <div className="flex items-start ml-[100px] mt-[10px]">
+           <Image src="/images3/$212.png" alt="rates" width={53} height={32}/>
+           <Image src="/images3/$242.png" alt="rates" width={58} height={32} className="ml-[10px]"/>
+           <Image src="/images3/off.png" alt="rates" width={58} height={32} className="ml-[10px]"/>
+           <Image src="/images2/rate6.png" alt="rates" width={54} height={32} className="ml-[120px]"/>
+           <Image src="/images2/rate3.png" alt="rates" width={56} height={32} className="ml-[260px]"/>
+           <Image src="/images2/rate1.png" alt="rates" width={188} height={32} className="ml-[265px] mt-[-5px]"/>
+           </div>
+           <Foot />
+           </div>
+    )
+}export default Detail
